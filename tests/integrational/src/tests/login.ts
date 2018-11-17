11b179824eca7748a1c12b9bb6b8b565ea9af384
EKorobov <arhaelk@gmail.com>
2018-11-17 17:35:27 +0200

    generate agent account test

--- tests/integrational/src/tests/login.ts
+++ tests/integrational/src/tests/login.ts
@@ -7,21 +7,54 @@ import { WS } from './../utils/ws';
 const configs = require('../configs/config.json');
 
 describe('first login', () => {
-  let agent: WS;
+  describe('configs file', () => {
+    it('should be read', () => {
+      expect(configs).to.not.be.undefined;
+    });
 
-  before('initialize websocket connection', async () => {
-    agent = new WS(configs['wsEndpoint'], client);
-
-    // wait for ws ready
-    await agent.whenReady();
+    it('should contain websocket endpoint property', () => {
+      expect(configs).to.have.property('agentWsEndpoint');
+    });
   });
 
-  it('should set agent password', async () => {
-    await agent.setPassword('hardcodedPasswd');
-  });
+  describe('websocket communication', () => {
+    let agent: WS;
 
-  it('should be zero accounts before test', async () => {
-    const accounts = await agent.getAccounts();
-    expect(accounts.length).to.equal(0);
+    before('initialize websocket connection', async () => {
+      agent = new WS(configs['agentWsEndpoint'], client);
+
+      // wait for ws ready
+      await agent.whenReady();
+    });
+
+    it('should set agent password', async () => {
+      await agent.setPassword('hardcodedPasswd');
+    });
+
+    describe('generating agent account', () => {
+      let accountId: string, accountRes: string;
+
+      it('should be zero accounts before generating', async () => {
+        const accounts = await agent.getAccounts();
+        expect(accounts.length).to.equal(0);
+      });
+
+      it('should generate new agent account', async () => {
+        const res = await agent.generateAccount({
+          isDefault: true,
+          inUse: true,
+          name: 'main'
+        });
+
+        accountId = res.id;
+        accountRes = res.result;
+      });
+
+      it('should contain already generated account', async () => {
+        const accounts = await agent.getAccounts();
+
+        expect(accounts.length).to.equal(1);
+      })
+    });
   });
 });
