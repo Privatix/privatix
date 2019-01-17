d2af8a366ab01877e40b07cdab03fa7567d3ac9e
Vitold55 <viktor@privatix.io>
2019-01-17 13:56:04 +0200

    Fix clear DB helper

--- tests/integrational/helpers/clear-db.sql
+++ tests/integrational/helpers/clear-db.sql
@@ -1,9 +1,12 @@
 --Delete all accounts and previously set passwords.
 --(this is required by )
-DELETE FROM accounts;
-DELETE FROM settings WHERE key='system.password';
 DELETE FROM settings WHERE key='system.salt';
-
---Delete all created offerings.
---(this is required by "Offering popup" tests)
+DELETE FROM settings WHERE key='system.password';
+DELETE FROM accounts;
+DELETE FROM endpoints;
+DELETE FROM sessions;
+DELETE FROM channels;
+DELETE FROM users;
 DELETE FROM offerings;
+DELETE FROM eth_txs;
+DELETE FROM jobs;
