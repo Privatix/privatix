a49fc57d1ae1ea3cc69124d166c08103b4098da9
Andrei Tseitlin <andrei@privatix.io>
2019-01-23 11:48:07 +0200

    fix toml generation seqence

--- build/win/build-helpers.psm1
+++ build/win/build-helpers.psm1
@@ -283,7 +283,8 @@ function Invoke-GoCommonOperations {
         [bool]$godep,
         [string]$branch,
         [bool]$gitpull,
-        [string]$gitUrl
+        [string]$gitUrl,
+        [string]$tomlTemplateFileName
     )
     $gitQuiet = "-q"
     if (($VerbosePreference -ne 'SilentlyContinue') -or ($PSBoundParameters.ContainsKey('Verbose')) ) {
@@ -349,6 +350,14 @@ function Invoke-GoCommonOperations {
     #endregion
 
     #region go dep
+    
+    #region Generate Gopkg.toml
+    if ($tomlTemplateFileName) {
+        . $PROJECT_PATH\scripts\win\toml.ps1 -templateFileName $tomlTemplateFileName -PROJECT_PATH $PROJECT_PATH
+        Copy-Item -Path "$PROJECT_PATH\$tomlTemplateFileName" -Destination "$PROJECT_PATH\Gopkg.toml" -ErrorAction Stop
+    }
+    #endregion
+    
     # Go dep
     If ($godep -and $GoDepInstalled) {
         try {
