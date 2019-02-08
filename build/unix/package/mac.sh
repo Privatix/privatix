57356493e3b1f565750d8f564d282a75d052829c
drew2a <for.drew@gmail.com>
2019-02-08 18:06:55 +0700

    patch settings.json
    for update feature

--- build/unix/package/mac.sh
+++ build/unix/package/mac.sh
@@ -64,6 +64,15 @@ create_gui_package(){
     rsync -azhP "${DAPP_GUI_DIR}/${DAPP_GUI_PACKAGE_MAC}/${DAPP_GUI_PACKAGE_MAC_BINARY_NAME}/." \
                 "${app_dir}/${DAPP_INSTALLER_GUI_DIR}/${DAPP_INSTALLER_GUI_BINARY_NAME}" || exit 1
 
+    # patch settings.json
+    python -c 'import json, sys
+with open(sys.argv[1], "r") as f:
+    obj = json.load(f)
+obj["release"]="'${VERSION_TO_SET_IN_BUILDER}'"
+obj["target"]="osx"
+with open(sys.argv[1], "w") as f:
+   json.dump(obj, f)' "${app_dir}/${DAPP_INSTALLER_GUI_DIR}/${DAPP_INSTALLER_GUI_BINARY_NAME}/${DAPP_GUI_SETTINGS_JSON}"
+
 }
 
 copy_product(){
