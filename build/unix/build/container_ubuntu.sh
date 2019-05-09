9b31cba6444bc44f8bc78bf347a708c9d7e91367
drew2a <for.drew@gmail.com>
2019-05-09 11:54:11 +0300

    add output for debug purposes

--- build/unix/build/container_ubuntu.sh
+++ build/unix/build/container_ubuntu.sh
@@ -28,50 +28,65 @@ sudo cp -R ${dappcoredir}/. "${container_location}/"
 # connect to container
 sudo systemd-nspawn -D ${container_location}/ << EOF
 
+echo -----------------------------------------------------------------------password
 #set root password: xHd26ksN
 echo -e "xHd26ksN\nxHd26ksN\n" | passwd
 
+echo -----------------------------------------------------------------------settings
 echo "pts/0" >> /etc/securetty
 echo deb http://deb.debian.org/debian stretch-backports main > /etc/apt/sources.list.d/stretch-backports.list
 
+echo -----------------------------------------------------------------------system
 apt-get update
 apt-get -t stretch-backports install -y systemd
 apt-get install -y dbus net-tools
 
-# install locale
+echo -----------------------------------------------------------------------locale
 apt-get install -y locales
-
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 update-locale LANG=en_US.UTF-8
 
-# install postgres
+echo -----------------------------------------------------------------------ca-certificates
 apt-get update
 apt-get install -y ca-certificates
 
+echo -----------------------------------------------------------------------postgress
 echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
 apt-get install -y postgresql-10
 
-# configure postgres
+echo -----------------------------------------------------------------------config-postgress
+
 service postgresql stop
-sed -i.bup 's/port = 5432/port = 5433/g' /etc/postgresql/10/main/postgresql.conf
+
+sed -i.bup 's/port = [0-9][0-9][0-9][0-9]/port = 5433/g' /etc/postgresql/10/main/postgresql.conf
+diff /etc/postgresql/10/main/postgresql.conf  /etc/postgresql/10/main/postgresql.conf.bup
+echo
+
 sed -i.bup 's/local.*all.*postgres.*peer/local all postgres trust/g' /etc/postgresql/10/main/pg_hba.conf
+diff /etc/postgresql/10/main/pg_hba.conf  /etc/postgresql/10/main/pg_hba.conf.bup
+echo
+
 sed -i.buh 's/host.*all.*all.*127.0.0.1\/32.*md5/host all postgres 127.0.0.1\/32 trust/g' /etc/postgresql/10/main/pg_hba.conf
+diff /etc/postgresql/10/main/pg_hba.conf  /etc/postgresql/10/main/pg_hba.conf.bup
+echo
+
 service postgresql start
 
-# install Tor
+echo -----------------------------------------------------------------------tor
 apt-get install -y tor
 service tor stop
 sed -i.bup 's/AppArmorProfile=system_tor/AppArmorProfile=/g' /lib/systemd/system/tor@default.service
 systemctl daemon-reload
 service tor start
 
-# enable dappctrl daemon
+echo -----------------------------------------------------------------------dappctrl
 mv /dappctrl/dappctrl.service /lib/systemd/system/
 systemctl enable dappctrl.service
 
+echo -----------------------------------------------------------------------remove
 rm -rf /usr/share/doc/*
 find /usr/share/locale -maxdepth 1 -mindepth 1 ! -name en_US -exec rm -rf {} \;
 find /usr/share/i18n/locales -maxdepth 1 -mindepth 1 ! -name en_US -exec rm -rf {} \;
