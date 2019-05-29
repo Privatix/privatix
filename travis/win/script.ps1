b35fd4b8fac49f2362cb084e4cce3b8c7d9de12a
Andrei Tseitlin <andrei@privatix.io>
2019-05-29 17:40:02 +0300

    test build

--- travis/win/script.ps1
+++ travis/win/script.ps1
@@ -1,1 +1,5 @@
-cd 
+cd $HOME\build\Privatix\privatix\build\win\
+mkdir $HOME\art
+wget http://artdev.privatix.net/artefacts_win.zip -OutFile $HOME\artefacts_win.zip
+Expand-Archive -Path $HOME\artefacts_win.zip -DestinationPath $HOME\art
+.\publish-dapp.ps1 -staticArtefactsDir $HOME\art -product vpn -gitpull -prodConfig -Verbose
