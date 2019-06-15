43af6abe7ba00e214d338684af8d3d356de109dc
Andrei Tseitlin <sofabeat@gmail.com>
2019-06-15 12:26:27 +0300

    move to script part

--- travis/install.sh
+++ travis/install.sh
@@ -49,16 +49,3 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
        "${BITROCK_INSTALLER_MAC}/license.xml" || exit 1
 fi
 
-#
-# windows
-#
-if [ "$TRAVIS_OS_NAME" = "windows" ]; then
-    powershell -Command 'mkdir c:\art'
-    powershell -Command 'wget http://artdev.privatix.net/artefacts_win.zip -outFile c:/art/artefacts_win.zip'
-    powershell -Command 'Expand-Archive -Path c:\art\artefacts_win.zip -DestinationPath C:\art'
-    powershell -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; wget -URI https://installbuilder.bitrock.com/installbuilder-enterprise-19.5.0-windows-x64-installer.exe -OutFile .\installbuilder-installer.exe -UseBasicParsing'
-    powershell -Command '.\installbuilder-installer.exe --mode unattended --prefix c:\installbuilder'
-    #powershell -File 'build\win\prepare-environment-travis.ps1'
-    cp "${TRAVIS_BUILD_DIR}/travis/encrypted/license.xml" \
-       "/mnt/c/installbuilder/license.xml" || exit 1
-fi
