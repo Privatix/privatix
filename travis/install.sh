aeab1b41588d304fa9d5e1dddfd3ebceb82346ff
Andrei Tseitlin <sofabeat@gmail.com>
2019-06-15 10:36:55 +0300

    comand by command

--- travis/install.sh
+++ travis/install.sh
@@ -53,7 +53,12 @@ fi
 # windows
 #
 if [ "$TRAVIS_OS_NAME" = "windows" ]; then
-    powershell -File 'build\win\prepare-environment-travis.ps1'
+    powershell -Command 'mkdir c:\art'
+    powershell -Command 'wget http://artdev.privatix.net/artefacts_win.zip -outFile c:/art/artefacts_win.zip'
+    powershell -Command 'Expand-Archive -Path c:\art\artefacts_win.zip -DestinationPath C:\art'
+    powershell -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; wget -URI https://installbuilder.bitrock.com/installbuilder-enterprise-19.5.0-windows-x64-installer.exe -OutFile .\installbuilder-installer.exe -UseBasicParsing'
+    powershell -Command '.\installbuilder-installer.exe --mode unattended --prefix c:\installbuilder'
+    #powershell -File 'build\win\prepare-environment-travis.ps1'
     cp "${TRAVIS_BUILD_DIR}/travis/encrypted/license.xml" \
        "/mnt/c/installbuilder/license.xml" || exit 1
 fi
