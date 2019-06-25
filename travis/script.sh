00eac258178e03237eea42a2c81c824045ba2336
Andrei Tseitlin <sofabeat@gmail.com>
2019-06-25 17:33:49 +0300

    import build.win.global.config

--- travis/script.sh
+++ travis/script.sh
@@ -19,6 +19,9 @@ fi
 # windows
 #
 if [ "$TRAVIS_OS_NAME" = "windows" ]; then    
+    cd "${TRAVIS_BUILD_DIR}/build/win"
+    . "./build.win.global.config"
+    
     powershell -Command "mkdir ${BUILD_WIN_DIR}"
     powershell -File 'build\win\publish-dapp.ps1' -wkdir "${BUILD_WIN_DIR}" -staticArtefactsDir "${ARTEFACTS_WIN_LOCATION}" -product vpn -installer -version ${VERSION_TO_SET_IN_BUILDER} -forceUpdate:${DAPP_INSTALLER_FORCE_UPDATE} -prodConfig -Verbose -dappguibranch "${GIT_BRANCH}" -dappctrlbranch "${GIT_BRANCH}" -dappinstbranch "${GIT_BRANCH}" -dappopenvpnbranch "${GIT_BRANCH}"
     #powershell -File 'build\win\publish-dapp.ps1' -wkdir "${BUILD_WIN_DIR}" -staticArtefactsDir "${ARTEFACTS_WIN_LOCATION}" -product proxy -installer -version ${VERSION_TO_SET_IN_BUILDER} -forceUpdate:${DAPP_INSTALLER_FORCE_UPDATE} -prodConfig -Verbose -dappguibranch "${GIT_BRANCH}" -dappctrlbranch "${GIT_BRANCH}" -dappinstbranch "${GIT_BRANCH}" -dappproxybranch "${GIT_BRANCH}"
