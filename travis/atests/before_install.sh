189cf9d85db1db6e534092e64ae4ccecf39c3e1c
Konstantin <shaytan4@gmail.com>
2019-09-09 16:07:05 +0300

    changes2

--- travis/atests/before_install.sh
+++ travis/atests/before_install.sh
@@ -3,14 +3,15 @@
 #
 echo -e ">>> some magic"
 
-echo -n $autotest1 >> ~/.ssh/travis_rsa_tests
-base64 --decode --ignore-garbage ~/.ssh/travis_rsa_tests > ~/.ssh/id_rsa
-
+echo -n $autotest1 >> /tmp/travis_rsa_tests
+base64 --decode --ignore-garbage /tmp/travis_rsa_tests > /tmp/id_rsa
+mkdir ~/.ssh
+ssh-add /tmp/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 echo -e ">>> Copy config"
-mv -fv out/.travis/ssh-config ~/.ssh/config
+#mv -fv out/.travis/ssh-config ~/.ssh/config
 
-ssh -T 89.38.99.85 hostname && wget http://ya.ru
+ssh -T stagevm@89.38.99.85 hostname && wget http://ya.ru
 #echo -e "LOGIN ssh"
 #wget http://ya.ru/
