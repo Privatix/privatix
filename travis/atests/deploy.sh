b7e6bf8155743d535be607754dee44a612245cf1
Konstantin <shaytan4@gmail.com>
2019-09-10 16:15:31 +0300

    fix17

--- travis/atests/deploy.sh
+++ travis/atests/deploy.sh
@@ -4,4 +4,12 @@
 #ssh stagevm@89.38.99.85 hostname && wget http://ya.ru
 #echo -e "LOGIN ssh"
 #wget http://ya.ru/
-cp ./bin/vpn/ubuntu/linux-dapp-installer/app.tar.xz stagevm@89.38.99.85:~
+
+#scp ./bin/vpn/ubuntu/linux-dapp-installer/app.tar.xz stagevm@89.38.99.85:~
+
+ssh stagevm@89.38.99.85 <<EOF
+mkdir test
+cd test
+wget http://ya.ru
+EOF
+
