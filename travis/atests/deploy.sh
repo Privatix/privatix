c046fe279768ce607dd6a6db3f115214a7763e21
Konstantin <shaytan4@gmail.com>
2019-09-13 15:08:11 +0300

    delete decode block

--- travis/atests/deploy.sh
+++ travis/atests/deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 # DOC - https://docs.privatix.network/support/install/cli-install-privatix-agent-node
-### delete this block V
-openssl aes-256-cbc -K $encrypted_b3625d5d910f_key -iv $encrypted_b3625d5d910f_iv \
-    -in ${TRAVIS_BUILD_DIR}/travis/encrypted.zip.enc \
-    -out ${TRAVIS_BUILD_DIR}/travis/encrypted.zip -d
-
-unzip ${TRAVIS_BUILD_DIR}/travis/encrypted.zip \
-      -d ${TRAVIS_BUILD_DIR}/travis/
-### delete this block ^
 
 cd "${TRAVIS_BUILD_DIR}/build/unix" || exit 1
 . "./build.global.config"
