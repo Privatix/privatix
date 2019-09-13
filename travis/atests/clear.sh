7804874adf9c00eaafe8279a33148858c5fd187a
Konstantin <shaytan4@gmail.com>
2019-09-13 10:56:37 +0300

    refactor clear.sh

--- travis/atests/clear.sh
+++ travis/atests/clear.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 # Clear before install
 
-agent="sudo ./dapp-installer remove -workdir /var/lib/container/agent/ -role agent"
-client="sudo ./dapp-installer remove -workdir /var/lib/container/client/ -role client"
-
 install_dir="/opt/privatix_installer"
 if [ -d "$install_dir" ]; then
-	echo "Installation present, prepare for clean"
-    cd /opt/privatix_installer &&
+      echo "Installation present, prepare for clean"
+    cd /opt/privatix_installer || exit 1
 
-    if [ "$1" == "agent" ]; then
-    echo "DEL AGENT"
-    $agent
-    fi
-    if [ "$1" == "client" ]; then
-    echo "DEL CLIENT"
-    $client
-    fi
+    echo Removing ${1}...
+    sudo ./dapp-installer remove -workdir /var/lib/container/${1}/ -role ${1}
 
     sudo rm -rf /opt/privatix_installer/
     sudo apt-get remove privatix -y
     sudo rm ~/Downloads/privatix*
 else
- 	echo "Nothing to do"
+     echo "Nothing to do"
 fi
