ca4c7fe4d3123ef6ce967c22ec1a440d5f52db31
drew2a <for.drew@gmail.com>
2019-09-18 12:59:37 +0300

    add fast build

---
+++ travis/fast_script.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+#
+# ubuntu & osx
+#
+if [ "$TRAVIS_OS_NAME" = "linux" ] || [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    cd "${TRAVIS_BUILD_DIR}/build/unix" || exit 1
+    . "./build.global.config"
+
+    git/update.sh || exit 1
+
+    build/dappctrl.sh || exit 1
+    build/dapp-installer.sh || exit 1
+    build/dapp-openvpn.sh || exit 1
+
+    [ "$TRAVIS_OS_NAME" = "linux" ] && output_folder=${VPN_UBUNTU_OUTPUT_DIR}
+    [ "$TRAVIS_OS_NAME" = "osx" ] && output_folder=${VPN_MAC_OUTPUT_DIR}
+
+    cp -v   "${GOPATH}/bin/${DAPPCTRL}" \
+            "${output_folder}/${DAPPCTRL}" || exit 1
+
+    cp -v "${GOPATH}/bin/${DAPP_SUPERVISOR}" \
+          "${output_folder}/${DAPP_SUPERVISOR}" || exit 1
+
+    cp -v "${GOPATH}/bin/${DAPP_INSTALLER}" \
+          "${output_folder}/${DAPP_INSTALLER}" || exit 1
+
+    cp -v "${GOPATH}/bin/${DAPP_OPENVPN}" \
+          "${output_folder}/${DAPP_OPENVPN}" || exit 1
+
+    cp -v "${GOPATH}/bin/${OPENVPN_INST}" \
+          "${output_folder}/${DAPP_INST}" || exit 1
+fi
+
+#
+# windows
+#
+#if [ "$TRAVIS_OS_NAME" = "windows" ]; then
+#    cd "${TRAVIS_BUILD_DIR}/build/win"
+#    . "./build.win.global.config"
+#
+#    powershell -File '.\publish-dapp.ps1' -wkdir "${BUILD_WIN_DIR}" -staticArtefactsDir "${ARTEFACTS_WIN_LOCATION}" -product VPN -installer -version "${VERSION_TO_SET_IN_BUILDER}" -forceUpdate "${DAPP_INSTALLER_FORCE_UPDATE}" -dappctrlConf "${DAPPCTRL_CONFIG}" -guiEthNetwork "${DAPP_GUI_NETWORK}" -dappguibranch "${GIT_BRANCH}" -dappctrlbranch "${GIT_BRANCH}" -dappinstbranch "${GIT_BRANCH}" -dappopenvpnbranch "${GIT_BRANCH}" -defaultBranch "${GIT_BRANCH_DEFAULT}" -asjobs -Verbose
+#fi
