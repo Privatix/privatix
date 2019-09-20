7117eba84023f8fadf1916c6db8c216e30956275
Timur Appaev <alloaolla@gmail.com>
2019-09-20 13:33:24 +0300

    tests: endpoints via ENV

--- tests/integrational/src/tests/init-tests/ws-initialization.spec.ts
+++ tests/integrational/src/tests/init-tests/ws-initialization.spec.ts
@@ -8,20 +8,25 @@ import { TestInputSettings } from '../../typings/test-models';
 
 export async function wsInitialization(settings: TestInputSettings) {
   const { configs } = settings;
+
+    const agentWsEndpoint = process.env['AGENT_WS_ENDPOINT'] ? process.env['AGENT_WS_ENDPOINT'] : configs.agentWsEndpoint;
+    const clientWsEndpoint = process.env['CLIENT_WS_ENDPOINT'] ? process.env['CLIENT_WS_ENDPOINT'] : configs.clientWsEndpoint;
+    const clientSupervisorEndpoint = process.env['CLIENT_SUPERVISOR_ENDPOINT'] ? process.env['CLIENT_SUPERVISOR_ENDPOINT'] : configs.clientSupervisorEndpoint;
+
   const scopes = getAllowedScope();
 
       this.timeout = 20000;
       if(scopes.includes('AGENT') || scopes.includes('BOTH') || scopes.includes('UNI')){
-          this.agentWs = new WS(configs['agentWsEndpoint']);
+          this.agentWs = new WS(agentWsEndpoint);
           settings.agentWs = this.agentWs;
           // wait for ws ready
           await this.agentWs.whenReady();
       }
 
       if(scopes.includes('CLIENT') || scopes.includes('BOTH') || scopes.includes('UNI')){
-            const res = await fetch(`${configs.clientSupervisorEndpoint}/start`);
+            const res = await fetch(`${clientSupervisorEndpoint}/start`);
             expect(res.status).to.be.equal(200);
-            this.clientWs = new WS(configs['clientWsEndpoint']);
+            this.clientWs = new WS(clientWsEndpoint);
             settings.clientWs = this.clientWs;
             // wait for ws ready
             await this.clientWs.whenReady();
