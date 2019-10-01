aa4c899d7153b65fd44a9c898c5cc26a457a10ba
Timur Appaev <alloaolla@gmail.com>
2019-10-01 17:03:18 +0300

    fix tests

--- tests/integrational/src/tests/smoke-tests/get-eth-prix.spec.ts
+++ tests/integrational/src/tests/smoke-tests/get-eth-prix.spec.ts
@@ -1,11 +1,14 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import {skipBlocks, getEth, getBlockchainTimeouts} from '../../utils/eth';
+import {/* skipBlocks, */ getEth, getBlockchainTimeouts} from '../../utils/eth';
+import { Until } from '../../utils/until';
+
 import {
   TestInputSettings,
   TestModel, TestScope
 } from '../../typings/test-models';
+
 
 export const getEthPrix: TestModel = {
     name: 'get testFn ETH and PRIX',
@@ -15,6 +18,9 @@ export const getEthPrix: TestModel = {
 
         describe('get testFn-models ETH and PRIX', () => {
             it(`should get Prixes on the ${allowedScope}  wallet`, async function () {
+
+                const until = Until(ws);
+
                 const bcTimeouts = getBlockchainTimeouts();
                 this.timeout(bcTimeouts.testTimeout);
 
@@ -23,7 +29,13 @@ export const getEthPrix: TestModel = {
                 const address = accounts[0].ethAddr;
 
                 await getEth(configs.getPrixEndpoint, TELEGRAM_BOT_USER, TELEGRAM_BOT_PASSWORD, address);
-                await skipBlocks(configs.timeouts.getEther.skipBlocks, ws, bcTimeouts.getEthTimeout, bcTimeouts.getEthTick);
+                // await skipBlocks(configs.timeouts.getEther.skipBlocks, ws, bcTimeouts.getEthTimeout, bcTimeouts.getEthTick);
+                await until
+                    .object('account')
+                    .withId(accounts[0].id)
+                    .prop('ethBalance')
+                    .becomes(balance => balance !== 0);
+
                 const accountsAfterTopup = await ws.getAccounts();
 
                 expect(accountsAfterTopup[0].ethBalance - accounts[0].ethBalance).to.equal(configs.getEth.ethBonus);
