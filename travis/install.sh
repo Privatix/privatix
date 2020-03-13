cd99cde3e7fcee86352a330887fb3c68c41eec18
drew2a <for.drew@gmail.com>
2020-03-13 14:29:24 +0800

    change bitrock source to github

--- travis/install.sh
+++ travis/install.sh
@@ -12,17 +12,16 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     # bitrock
     #
     # download
-    # https://installbuilder.bitrock.com/installbuilder-enterprise-19.5.0-linux-x64-installer.run
     set -x
-    downloading_binary=installbuilder-enterprise-${BITROCK_VERSION}-linux-x64-installer.run
 
-    wget  https://installbuilder.bitrock.com/${downloading_binary} || exit 1
+    wget "${BITROCK_INSTALLER_URL_LINUX}" || exit 1
 
-    chmod u+x ./${downloading_binary} || exit 1
+    downloading_binary=$(basename "${BITROCK_INSTALLER_URL_LINUX}")
+    chmod u+x "./${downloading_binary}" || exit 1
 
     # install
-    echo install: ./${downloading_binary} --mode unattended || exit 1
-    ./${downloading_binary} --mode unattended || exit 1
+    echo install: "./${downloading_binary}" --mode unattended || exit 1
+    "./${downloading_binary}" --mode unattended || exit 1
 
     set +x
     # add license
@@ -37,15 +36,13 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     # bitrock
     #
     # download
-    # https://installbuilder.bitrock.com/installbuilder-enterprise-19.5.0-osx-installer.dmg
     set -x
-    bitrock_installer=installbuilder-enterprise-${BITROCK_VERSION}-osx-installer
 
-    # curl -L -O  https://installbuilder.bitrock.com/installbuilder-enterprise-19.5.0-osx-installer.dmg
-    curl -LOf https://installbuilder.bitrock.com/${bitrock_installer}.dmg || exit 1
+    curl -LOf "${BITROCK_INSTALLER_URL_MAC}" || exit 1
+    bitrock_installer=$(basename "${BITROCK_INSTALLER_URL_MAC}" .dmg)
 
     # mount
-    hdiutil attach -mountpoint ${bitrock_installer} ${bitrock_installer}.dmg || exit 1
+    hdiutil attach -mountpoint "${bitrock_installer}" "${bitrock_installer}.dmg" || exit 1
 
     # install
     "./${bitrock_installer}/${bitrock_installer}.app/Contents/MacOS/installbuilder.sh" --mode unattended || exit 1
@@ -63,7 +60,7 @@ if [ "$TRAVIS_OS_NAME" = "windows" ]; then
     powershell -Command 'Expand-Archive -Path c:\art\artefacts_win.zip -DestinationPath c:\art'
     # download and install Bitrock installer
     powershell -Command 'mkdir c:\installbuilder'
-    powershell -Command 'curl.exe -L -O -f "https://installbuilder.bitrock.com/installbuilder-enterprise-'${BITROCK_VERSION}'-windows-x64-installer.exe" --output .\installbuilder-installer.exe'
+    powershell -Command 'curl.exe -L -O -f "https://github.com/Privatix/privatix/releases/download/1.2.0/installbuilder-enterprise-'${BITROCK_VERSION}'-windows-x64-installer.exe" --output .\installbuilder-installer.exe'
     powershell -Command '.\installbuilder-installer.exe --mode unattended --prefix c:\installbuilder'
     # add license to Bitrock installer
     powershell -Command 'Copy-Item -Path "c:\Users\travis\gopath\src\github.com\Privatix\privatix\travis\encrypted\license.xml" -Destination "c:\installbuilder\"'
